e461999c0fbf63e49003268bf95c74a3695dd258
10994015 <a0938599191@gmail.com>
2022-12-16 22:19:40 +0800

    1216

---
+++ js/app.js
@@ -0,0 +1,25 @@
+const header = document.getElementById('header');
+const menuBar = document.getElementById('menu-bar');
+let isMenuOpen = false;
+window.addEventListener('scroll', ()=>{
+    if(this.scrollY > 100){
+        header.classList.add('active')
+    }else{
+        header.classList.remove('active')
+    }
+    if(this.scrollY > 500){
+        totop.classList.add('active')
+    }else{
+        totop.classList.remove('active')
+    }
+})
+menuBar.addEventListener('click',()=>{
+    isMenuOpen = !isMenuOpen;
+    if(isMenuOpen){
+        nav.style.top = "0";
+        menuBar.classList.add('fa-times');
+    }else{
+        nav.style.top = "-100%";
+        menuBar.classList.remove('fa-times');
+    }
+})
